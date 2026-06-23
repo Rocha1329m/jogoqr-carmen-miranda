82f7592f6236c46d0e679f6e426d279e7158681b
Rocha1329m <martimrocha1329@gmail.com>
2026-06-23 09:16:16 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,14 @@
-<script>
+function verificar(correta){
 
-let respondido = false;
-
-function verificar(correta, botao){
-
-    if(respondido) return;
-
-    respondido = true;
-
-    const resultado = document.getElementById("resultado");
-    const botoes = document.querySelectorAll("button");
-
-    botoes.forEach(btn => {
-        btn.disabled = true;
-        btn.style.opacity = "0.7";
-        btn.style.cursor = "not-allowed";
-    });
+    let resultado =
+        document.getElementById("resultado");
 
     if(correta){
         resultado.innerHTML = "✅ Correto!";
-        resultado.style.color = "#7CFF8A";
-
-        botao.style.background = "#7CFF8A";
-        botao.style.color = "#000";
+        resultado.style.color = "green";
     }
     else{
         resultado.innerHTML = "❌ Errado!";
-        resultado.style.color = "#ff2c2c";
-
-        botao.style.background = "#ff2c2c";
-        botao.style.color = "#fff";
+        resultado.style.color = "red";
     }
 }
-
-</script>
